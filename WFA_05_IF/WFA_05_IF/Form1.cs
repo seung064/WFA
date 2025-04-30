b2ad08ca9e66866c0895b2934a07d3cd897f8205
seung064 <gli056@naver.com>
2025-04-30 20:37:33 +0900

    input

--- WFA_05_IF/WFA_05_IF/Form1.cs
+++ WFA_05_IF/WFA_05_IF/Form1.cs
@@ -16,6 +16,20 @@ namespace WFA_05_IF
         {
             InitializeComponent();
 
+            /*
+            • 동전 던지기(앞 면 또는 뒷 면) 함수를 작성
+            1. 함수 이름을 적당하게 짓기
+            2. 출력 자료형은 bool
+            3. 입력은 bool 형 1개
+            4. “난수 생성”을 검색하여 난수 생성 방법을 학습하고 int형 난수를 생성
+            5. 생성된 난수와 % 연산을 이용, 연산 결과로 0 또는 1만 값이 나오도록 작성
+            6. 입력 값과 연산 결과를 비교하여 둘이 같으면 true를 반환, 다르면 false를 반환
+            • 1 = true, 0 = false 라고 가정
+            • 함수에 true 또는 false를 입력하고 반환된 결과에 따라 “승리” 또는 “패배”로
+            TextBox에 표시
+
+            */
+
             if (coin())
             {
                 textBox1.Text = "승리";
@@ -32,11 +46,6 @@ namespace WFA_05_IF
         }
       
 
-        int Add(int num)
-        {
-            int a = num % 2;
-            return a;
-        }
 
         bool coin()
         {
---
+++ WFA_06_Input/WFA_06_Input/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_06_Input
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        #region #1-3 실습) 사용자 입력 받기 - 이벤트 처리
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = textBox_input.Text; // 입력값을 결과창에 먼저 복사
+
+            bool input = false;
+
+            if (textBox_input.Text.Length > 0)
+            {
+                string inputText = textBox_input.Text;
+
+                if (!(inputText == "true" || inputText == "false"))
+                {
+                    MessageBox.Show("true 또는 false를 입력하세요.");
+                    return;
+                }
+                else
+                {
+                    input = bool.Parse(inputText);
+                }
+            }
+
+            else if (radioButton_true.Checked)
+            {
+                input = true;
+            }
+            else if (radioButton_false.Checked)
+            {
+                input = false;
+            }
+
+            textBox_result.Text = "입력하신 값은 " + input.ToString() + "입니다! \r\n";
+            textBox_result.Text += "동전 던지기 결과... \r\n";
+
+            string gameResultMessage;
+
+            if (CoinMatch(input))
+            {
+                gameResultMessage = "승리 ~!";
+            }
+            else
+            {
+                gameResultMessage = "패배 ..ㅠ";
+            }
+
+            textBox_result.Text += gameResultMessage + "\r\n";
+        }
+        #endregion
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_input_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private bool CoinMatch(bool userInput)
+        {
+            Random rnd = new Random();
+            bool coin = rnd.Next(2) == 0; // true 또는 false 랜덤 생성
+            return userInput == coin;
+        }
+    }
+}
+
